8a30ff1898e42dbe9811cd29fc82193fbc850f47
José Monar <jlmonar@espol.edu.ec>
2017-10-06 01:14:27 -0500

    Initial commit.

---
+++ src/test/java/com/platzi/springplatziprofesores/SpringplatziprofesoresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.platzi.springplatziprofesores;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringplatziprofesoresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
